787fb95b438aff46e837caf2f5c550a85c50d207
CArmenta5 <42613070+CArmenta5@users.noreply.github.com>
2021-05-26 16:35:38 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  res.end('Hello World, testing the pull at github & checking now the CRONTAB x2!\n');
+  res.end('Hello World, testing the pull at github & checking now the CRONTAB x3!\n');
 });
 
 server.listen(port, hostname, () => {
