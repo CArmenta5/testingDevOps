ff11197521cb1f538b108ee0c20639ef44db7b41
carmenta5 <carlosraulap@outlook.com>
2021-05-26 16:12:43 -0500

    Changes to use cron with a Ruby script

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, testing the pull at github!\n');
+  res.end('Hello World, testing now just, please x4\n');
 });
 
 server.listen(port, hostname, () => {
--- hola.rb
+++ hola.rb
@@ -1,1 +1,24 @@
-puts"Hola este es mi primer scrit en ruby"
+require 'fileutils'
+
+
+firstFile = File.basename("/home/carmenta/testingDevOps/hello.js");
+secondFIle = File.basename("/home/carmenta/testingDevOps/helloCopia.js");
+
+
+flag = FileUtils.compare_file(firstFile, secondFIle);
+
+if flag
+    puts "Archivos iguales"
+    #puts "Eliminando archivos desactualizados del servidor"
+
+    #puts "Creación de server con documento actualizado"
+else
+    puts "Archivos no iguales"
+    wasGood = system("/usr/bin/node /usr/local/bin/pm2 restart hello.js");
+
+
+   
+
+end #
+
+
