f13a10dbcba65184668a834742dc2ed3a827333e
CArmenta5 <42613070+CArmenta5@users.noreply.github.com>
2021-05-26 02:06:05 -0500

    Crontab

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, testing the pull at github!\n');
+  res.end('Hello World, testing the pull at github & checking now the CRONTAB!\n');
 });
 
 server.listen(port, hostname, () => {
